8a5b3552b4c97dbb99749656d2d258206239a899
Igor Zibarev <zibarev.i@gmail.com>
2016-08-31 19:36:26 +0700

    [apps] Improve node installation

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -12,10 +12,19 @@ apps::nodejs-n() {
         return 0
     fi
 
-    echo "Installing node.js-n"
+    std::info "Install node.js-n"
     git clone https://github.com/tj/n ${HOME}/sources/tj/n
     cd ${HOME}/sources/tj/n
     PREFIX=${HOME}/apps/n make install
+
+    # Install latest node and npm packages
+    std::info "Install latest Node.js and global npm packages"
+    export N_PREFIX="${HOME}/apps/n"
+    export PATH="$PATH:$N_PREFIX/bin"
+    n latest
+    npm install -g \
+        karma-cli \
+        nodemon
 }
 
 apps::mongodb() {
