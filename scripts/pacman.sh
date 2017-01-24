287633bbc69b0e0a9ffd98bc2fc929357cc49439
Igor Zibarev <zibarev.i@gmail.com>
2017-01-24 19:31:54 +0700

    [pacman] fix mirrors

--- scripts/pacman.sh
+++ scripts/pacman.sh
@@ -23,7 +23,7 @@ pacman::update_keys() {
 }
 
 pacman::update_mirrors() {
-    if grep "Location" /etc/pacman.d/mirrorlist | head -n1 | grep -q "Russia"
+    if grep "Country" /etc/pacman.d/mirrorlist | head -n1 | grep -q "Russia"
     then
         return 0
     fi
